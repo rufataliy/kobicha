9401f895c9563acf32a72f40af72206a0997b2c6
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-05 15:13:06 -0500

    Created home page index

---
+++ kobicha/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import {  Hero } from "../components";
+interface Props {
+  data: Data;
+}
+
+const Home: React.FC<Props> = ({ data }) => {
+  return (
+    <>
+    Home
+    </>
+  );
+};
+
+export default Home;
