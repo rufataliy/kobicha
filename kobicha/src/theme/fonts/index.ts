b0687438347c3fa418e2986b05af726bf94fda87
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-05 19:54:45 -0500

    Added local fonts

---
+++ kobicha/src/theme/fonts/index.ts
@@ -0,0 +1,12 @@
+export { default as raleway_medium_woff } from "./raleway-medium-webfont.woff";
+export { default as raleway_medium_woff2 } from "./raleway-medium-webfont.woff2";
+export { default as raleway_light_woff } from "./raleway-light-webfont.woff";
+export { default as raleway_light_woff2 } from "./raleway-light-webfont.woff2";
+export { default as raleway_semibold_woff } from "./raleway-semibold-webfont.woff";
+export { default as raleway_semibold_woff2 } from "./raleway-semibold-webfont.woff2";
+export { default as raleway_thin_woff } from "./raleway-thin-webfont.woff";
+export { default as raleway_thin_woff2 } from "./raleway-thin-webfont.woff2";
+export { default as raleway_italic_woff } from "./raleway-italic-webfont.woff";
+export { default as raleway_italic_woff2 } from "./raleway-italic-webfont.woff2";
+export { default as raleway_regular_woff } from "./raleway-regular-webfont.woff";
+export { default as raleway_regular_woff2 } from "./raleway-regular-webfont.woff2";
