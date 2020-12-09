5a01dbde2338bf9824094cb476e9bcb10e47b30e
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-09 18:46:23 -0500

    Added gap after wrapper

--- kobicha/src/components/commons/Wrapper.ts
+++ kobicha/src/components/commons/Wrapper.ts
@@ -3,17 +3,27 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
-  // background: lightblue;
+  max-width: 1266px;
   margin: 0 auto;
 
+  &:not(:first-child),
+  &:not(:last-child) {
+    margin-bottom: ${({ theme }) => theme.gaps.lg};
+  }
+
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    width: 100%;
+    width: 95%;
   }
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
     width: 90%;
   }
-
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.laptop}) {
+    &:not(:first-child),
+    &:not(:last-child) {
+      margin-bottom: ${({ theme }) => theme.gaps.xlg};
+    }
+  }
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
     width: 75%;
   }
